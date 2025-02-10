dd5cd4ac1b160adaa133883e4d2baafabd9a55d0
alastorid <alastorid@gmail.com>
2025-02-09 23:34:02 -0500

    up

--- examples/yt-txt.sh
+++ examples/yt-txt.sh
@@ -129,22 +129,33 @@ v="${source_url#*v=}"
 v="${v%%&*}"
 out_file_name="yt${v}"
 out_txt="${out_file_name}.txt"
+# Check if the input is a file or a URL
+if [ -f "$source_url" ]; then
+    # Input is a file
+    file_name=$(basename -- "$source_url")
+    out_file_name="${file_name%.*}"
+    out_txt="${out_file_name}.txt"
 
-VIDEO_ID=${v}
-URL="https://www.youtube.com/watch?v=${VIDEO_ID}"
-TITLE=$(yt-dlp --get-title "$URL")
-UPLOAD_DATE=$(yt-dlp --print upload_date "$URL")
-FORMATTED_DATE=$(date -j -f "%Y%m%d" "$UPLOAD_DATE" +"%Y.%m.%d")
+    if [ ! -f "$out_txt" ]; then
+        ffmpeg -hide_banner -loglevel error -i "$source_url" \
+            -af silenceremove=start_periods=1:start_duration=0:start_threshold=-50dB:stop_periods=1:stop_duration=0.7:stop_threshold=-50dB \
+            -ar 16000 -ac 1 -c:a pcm_s16le -f wav - \
+        | "$WHISPER_EXECUTABLE" -bs 6 -np -fa -m "$MODEL_PATH" -l "$WHISPER_LANG" -f - -t "$WHISPER_THREAD_COUNT" | tee "$out_txt"
+    fi
+else
+    VIDEO_ID=${v}
+    URL="https://www.youtube.com/watch?v=${VIDEO_ID}"
+    TITLE=$(yt-dlp --get-title "$URL")
+    UPLOAD_DATE=$(yt-dlp --print upload_date "$URL")
+    FORMATTED_DATE=$(date -j -f "%Y%m%d" "$UPLOAD_DATE" +"%Y.%m.%d")
 
-################################################################################
-# Download the video, put the dynamic output filename into a variable.
-# Optionally add --cookies-from-browser BROWSER[+KEYRING][:PROFILE][::CONTAINER]
-# for videos only available to logged-in users.
-################################################################################
-if [ ! -f ${out_txt} ] ; then
-    yt-dlp -f "bestaudio[ext=m4a]" -q --no-warnings --no-part -o - "${URL}" \
-        | ffmpeg -hide_banner -loglevel error -i - -af silenceremove=1:0:-50dB  -ar 16000 -ac 1 -c:a pcm_s16le -f wav - \
-        | "${WHISPER_EXECUTABLE}" -bs 6 -np -fa -m "${MODEL_PATH}" -l "${WHISPER_LANG}" -f - -t "${WHISPER_THREAD_COUNT}" | tee ${out_txt}
+    if [ ! -f ${out_txt} ] ; then
+        yt-dlp -f "bestaudio[ext=m4a]" -q --no-warnings --no-part -o - "${URL}" \
+            | ffmpeg -hide_banner -loglevel error -i - \
+                  -af silenceremove=start_periods=1:start_duration=0:start_threshold=-50dB:stop_periods=1:stop_duration=0.7:stop_threshold=-50dB \
+                  -ar 16000 -ac 1 -c:a pcm_s16le -f wav - \
+            | "${WHISPER_EXECUTABLE}" -bs 6 -np -fa -m "${MODEL_PATH}" -l "${WHISPER_LANG}" -f - -t "${WHISPER_THREAD_COUNT}" | tee ${out_txt}
+    fi
 fi
 
 p="${FORMATTED_DATE} ${TITLE}\n-------\n請列出此文稿中的所有關鍵要點及相關訊息，並對每個要點進行詳細描述。請特別注意以下事項：書名、電影名稱、提及的股票及其分析（包括何時看多或看空，並提供具體的分析理由）。摘錄全部有具體價值觀的原句。最後，請以表格的形式清楚地呈現財務及金融重點資訊，確保內容清晰易懂。"
